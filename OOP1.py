748a093d5c340922be1a8e11254377b7a792df46
gaurav-kshirsagar <gskram111@gmail.com>
2024-02-08 15:59:19 +0530

    adding OOP day1

---
+++ OOP1.py
@@ -0,0 +1,26 @@
+#OOPs (Object oriented program)
+
+class Student:
+    rn = 0
+    name = ''
+    marks = 0.0
+
+s1 = Student()
+s1.rn = 1
+s1.name = "Gaurav"
+s1.marks = 90
+
+print("s1.rn=",s1.rn)
+print("s1.name=",s1.name)
+print("s1.marks=",s1.marks)
+
+print()
+
+s2 = Student()
+s2.rn = 2
+s2.name = "abc"
+s2.marks = 80
+
+print("s1.rn=",s2.rn)
+print("s1.name=",s2.name)
+print("s1.marks=",s2.marks)
